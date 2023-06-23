96b1e8c42f3e7f868e4459391a61a19de6360a36
Mikhail Gorbonosov <mikesplace"mail.ru>
2023-06-23 14:32:28 +0300

    Passes module

--- src/api/v1/helpers/db.ts
+++ src/api/v1/helpers/db.ts
@@ -1,15 +1,20 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: 'db/sqlite.db',
+const database = process.env.POSTGRES_DATABASE;
+const username = process.env.POSTGRES_USERNAME;
+const password = process.env.POSTGRES_PASSWORD;
+const host = process.env.POSTGRES_HOST;
+
+const sequelize = new Sequelize(database, username, password, {
+  host,
+  dialect: 'postgres',
 });
 
 sequelize
   .authenticate()
   .then(() => {
     // eslint-disable-next-line no-console
-    console.info('Connected to sqlite');
+    console.info('Connected to postgres');
   })
   .catch((err) => {
     // eslint-disable-next-line no-console
